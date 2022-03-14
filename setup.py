5405ab3840c78191f68500912713079d0e83c73f
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-03-14 18:16:11 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
 from setuptools import setup
 
 setup(
-
     name = "usa",
-
     version = "0.1.0",
-
-    description = "Prints the USA flag in terminal",
-
-    author = "AitzazImtiaz",
-
-    url = "https://github.com/AitzazImtiaz/usa-flag-terminal",
-
-
+    description = "Prints USA flag",
+    author = "JeffTheK",
+    url = "https://github.com/Terminal-Patriotism/USA-flag-terminal",
+    packages = ["usa"],
     entry_points = {
-
         'console_scripts': [
-
             'usa = usa.__main__'
-
         ]
-
     },
-
 )
